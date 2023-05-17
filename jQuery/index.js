63300d3111bbd18b7006a23060713dc65a32e707
Nanditha P <nandithanandu57@gmail.com>
2023-05-17 11:20:00 +0530

    Adding and removing elements in jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,11 +1,21 @@
 //IMPLEMENTING jQUERY CODE 
 
 
-/*($("button").click(function () {
+/*$("button").click(function () {
     $("h1").css("color", "purple");
 });*/
 
 //keypress event
-$("input").keypress(function (event) {
-    console.log(event.key);
-})
+/*$(document).keypress(function (event) {
+    $("h1").text(event.key);
+});*/
+
+//Mouse over event
+
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "green");
+});
+//Adding and removing in jQuery
+$("h1").before("<button>New</button>");
+
+//after and prepend and append
