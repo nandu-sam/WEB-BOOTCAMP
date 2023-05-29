c7d05e09da290fe084ba512b6a87b723454d5357
Nanditha P <nandithanandu57@gmail.com>
2023-05-29 11:26:29 +0530

    practice of npm and node js

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,1 +1,11 @@
-console.log("Howdy world!");
+/*var superheroes = require("superheroes");
+var mySupeheroeName = superheroes.random();
+
+console.log(mySupeheroeName);*/
+
+
+
+var supervillians = require("supervillains");
+var mySupervilliansName = supervillians.random();
+
+console.log(mySupervilliansName);
