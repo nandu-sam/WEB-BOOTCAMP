20823050b967c9c16e4eb5fd922383a6cb352c2a
Nanditha P <nandithanandu57@gmail.com>
2023-05-30 08:34:27 +0530

    Project with express js

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,11 +1,11 @@
-/*var superheroes = require("superheroes");
-var mySupeheroeName = superheroes.random();
-
-console.log(mySupeheroeName);*/
-
-
-
+var superheroes = require("superheroes");
 var supervillians = require("supervillains");
+var mySupeheroeName = superheroes.random();
 var mySupervilliansName = supervillians.random();
-
+console.log(mySupeheroeName);
 console.log(mySupervilliansName);
+
+
+
+
+
