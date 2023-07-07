674d42857c81f1b811f911f72db98f87f6399da9
Nanditha P <nandithanandu57@gmail.com>
2023-07-07 11:50:11 +0530

    The complete To-Do app

--- Fruits-Project/app.js
+++ Fruits-Project/app.js
@@ -1,145 +1,127 @@
-// ----- 1st cmd ----- 
-// (1)npm init -y 
-// (2)npm i mongodb
- 
-// ----- 2nd cmd ----- 
-// (1)mongod
- 
-/*const { MongoClient } = require("mongodb");
- 
-// Connection URL (Replace the url string with your connection string.)
-// const url = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
-const url = "mongodb://localhost:27017";
- 
-// Create a new MongoClient
-const client = new MongoClient(url);
- 
-async function main() {
-  try {
-    // ----- Use connect method to connect to the server ----- 
-    await client.connect();
-    console.log('Connected successfully to server');
-    
-    // ----- Database Name ----- 
-    const database = client.db('fruitsDB');
-    // ----- Create a collection ----- 
-    const fruitsCollection = database.collection('fruits');
- 
-    // ----- Insert a document (data in row) -----
-    // Create an array of documents to insert
-    const docs = [
-        {
-            name: "Apple",  
-            score: 8, 
-            review: "Great Fruit"},
-        {   
-            name: "Orange", 
-            score: 6, 
-            review: "Kinda Sour"},
-        {
-            name: "Banana", 
-            score: 9, 
-            review: "Great Stuff"}
-       ];
-     
-    const insertResult = await fruitsCollection.insertMany(docs);
-    console.log("Inserted " + insertResult.insertedCount + " documents into the fruitsCollection.");
-    
-    // ----- 3rd cmd (to check whether the data is in mongosh) -----  
-    // (1)mongosh 
-    // (2)show dbs 
-    // (3)use <databseName>
-    // (4)show collections
-    // (5)db.<collectionName>.find()
-    
-    // ----- Find all documents -----
-    const findResult = await fruitsCollection.find({}).toArray();
-    console.table(findResult);
- 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
- 
-main().catch(console.dir);*/ //the above code you see is using node js native driver method
-
-//the below code is using moongose 
+//require the Mongoose package (after running >npm i mongoose in Hyper to install it)
 const mongoose = require('mongoose');
  
-//Connection to MongoDB database
-//⁡⁢⁣⁣This line will specify the port where we will access our MongoDB Server
-//⁡⁢⁣⁣Here "fruitsDB" is the name of the database where we want to connect to.⁡
-mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
+//connect to MongoDB by specifying port to access MongoDB server
+main().catch(err => console.log(err));
  
-//Here we create new blueprint of our database(Schema)
-//This lays foundation for every new fruit document that will be added to our DB.
-const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
-    review: String
-});
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/FruitsDB');
+  }
  
-const Fruit = mongoose.model("Fruit", fruitSchema);
- 
-const fruit = new Fruit({
-    name: "Apple",
-    rating: 7,
-    review: "Apple keeps the doctor away"
+//create a SCHEMA that sets out the fields each document will have and their datatypes
+const fruitSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true, "no name entered :("]
+  },
+  rating: {
+    type: Number,
+    min :1 ,
+    max : 10
+  }, 
+  review: String
 })
  
-//This save() method in Mongoose is used to save this fruit document into fruit collection inside our fruitsDB
-// fruit.save();//Once a collection is saved comment fruit.save() bcoz everytime it will save same thing multiple times.
+//create a MODEL
+const Fruit = new mongoose.model ("Fruit", fruitSchema)
  
+// //create a DOCUMENT
+// const fruit = new Fruit ({
+//  name: "Apple",
+//  rating: 37,
+//  review: "Great!"
+// })
+ 
+ 
+//save the document
+// fruit.save()
+ 
+//**CHALLENGE: Set up a people database with one document and two fields**//
+//create a SCHEMA
 const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+  name: String,
+  age: Number,
 });
  
-const Person = mongoose.model("Person", personSchema);
+//create a MODEL
+const Person = mongoose.model('Person', personSchema);
  
+//create a DOCUMENT
 const person = new Person({
-    name: "Jhon",
-    age: 37
+  name: "John",
+  age: 37
 });
  
-//Guys once you save this person_collection please comment it. Otherwise it will save the same thing multiple times.
-person.save();
+//Save it
+// person.save();
  
+//adding to fruits model
+const kiwi = new Fruit ({
+  name  : "kiwi",
+  rating: 7,
+  review : "kiwi brate"
+}); 
  
-//How to insert these many fruits at a time? (by using insertMany() method)
-const kiwi = new Fruit({
-    name: "Kiwi",
-    rating: 10,
-    review: "The Best Fruit!"
+const orange = new Fruit ({
+  name  : "orange",
+  rating: 6,
+  review : "kissljit"
 });
  
-const orange = new Fruit({
-    name: "Orange",
-    rating: 6,
-    review: "The Sour Fruit!"
-});
+const banana = new Fruit ({
+  name  : "banana",
+  rating: 10,
+  review : "za zapek !"
+}); 
  
-const banana = new Fruit({
-    name: "Banana",
-    rating: 8,
-    review: "The Digestive Fruit!"
-});
+//new way of inserting to db:
+// Fruit.insertMany([kiwi, orange, banana])
+//   .then(function() {
+//     console.log("Successfully added to your database!");
+//   })
+//   .catch(function(error) {
+//     console.log("Error adding fruits to the database: " + error);
+//   });
  
-//In latest version of mongoose insertMany has stopped accepting callbacks
-//instead they use promises(Which Angela has not taught in this course)
-//So ".then" & "catch" are part of PROMISES IN JAVASCRIPT.
+  Fruit.find()
+  .then(function(fruits) {
+    fruits.forEach(function(fruit) {
+      console.log(fruit.name);
  
-//PROMISES in brief(If something is wrong please correct me):
-//In JS, programmers encountered a problem called "callback hell", where syntax of callbacks were cumbersome & often lead to more problems.
-//So in effort to make it easy PROMISES were invented.
-//to learn more about promise visit : https://javascript.info/promise-basics
-//Or https://www.youtube.com/watch?v=novBIqZh4Bk
- 
-Fruit.insertMany([kiwi, orange, banana]) 
-    .then(function(){
-        console.log("Successfully saved all the fruits to fruitsDB");
-    })
-    .catch(function(err){
-        console.log(err);
+      mongoose.connection.close();
     });
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+ 
+ 
+  //updating the data
+   /*Fruit.updateOne({ _id: "640cd580cf502b8c60d9740e"}, { name: "Peach" })
+   .then(function(result) {
+    console.log("Successfully updated the fruit.");
+   })
+  .catch(function(err) {
+     console.log(err);
+   });*/
+ 
+ 
+  //delete only one 
+   Fruit.deleteOne({ name: "Peach" })
+   .then(function(result) {
+     console.log("Successfully deleted the fruit.");
+  })
+   .catch(function(err) {
+     console.log(err);
+   });
+ 
+ 
+  //delete many at once
+  // Person.deleteMany({ name: "John"})
+  // .then(function(result) {
+  //   console.log("Successfully deleted the all people.");
+  // })
+  // .catch(function(err) {
+  //   console.log(err);
+  // });
+  
