b4f9ac262297a12de083f13d5af7b2e4be797645
Nanditha P <nandithanandu57@gmail.com>
2023-07-31 13:38:27 +0530

    useState Example

--- my-app/src/Data.js
+++ my-app/src/Data.js
@@ -3,6 +3,9 @@ import datas from "./datas.json";
 
 const Data = () => {
   const [nData, setnData] = useState(datas);
+  const handleRemove = (itemId) => {
+    setnData(nData.filter((item) => item.id !== itemId));
+  };
 
   /*const handleClick=()=>{
     setnData([])
@@ -12,7 +15,11 @@ const Data = () => {
     <div>
       <ul>
         {nData.map((items) => (
-          <li key={items.id}>{items.name}</li>
+          <li key={items.id}>
+            {items.name}
+            <br />
+            <button onClick={() => handleRemove(items.id)}>Remove</button>
+          </li>
         ))}
       </ul>
       <button onClick={() => setnData([])}>Clear</button>
