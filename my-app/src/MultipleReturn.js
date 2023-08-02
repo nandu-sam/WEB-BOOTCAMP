e21e7aee9ffc939cc502e98cd71f2e28d459ec3e
Nanditha P <nandithanandu57@gmail.com>
2023-08-02 10:55:38 +0530

    multi

---
+++ my-app/src/MultipleReturn.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+const MultipleReturn = () => {
+  const [user, setUsers] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch("https://api.github.com/users");
+        const data = await response.json();
+        setUsers(data);
+        setisLoading(false);
+      } catch (error) {
+        setError(error);
+        setisLoading(false);
+      }
+    }
+    fetchUsers();
+  }, []);
+  if (isLoading) {
+    return <p>Loading Please Wait...</p>;
+  }
+  if (error) {
+    return <p>Error:{error.message}</p>;
+  }
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {user.map((user) => {
+          return (
+            <li key={user.id}>
+              <a href={user.html_url}>{user.login}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default MultipleReturn;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //stateless functional component
-import Fetch from "./Fetch.js";
+import MultipleReturn from "./MultipleReturn.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //method create root method in react
 
 root.render(
   <React.StrictMode>
-    <Fetch />
+    <MultipleReturn />
   </React.StrictMode> //this is a developer mode
 );
